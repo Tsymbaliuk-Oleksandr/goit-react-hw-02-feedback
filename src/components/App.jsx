5c79d2f484c37520d05ea0d764979c602acf9673
Oleksandr <103380042+Tsymbaliuk-Oleksandr@users.noreply.github.com>
2022-11-20 18:25:50 +0200

    goit-react-hw-02-feedback(end)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,69 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from "react";
+import { Component } from "react";
+import { Box } from "./Box";
+import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
+import { Notification } from "./Notification/Notification";
+import { Section } from "./Section/Section";
+import { Statistics } from "./Statistics/Statistics";
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleChosenFeedback = ({ option }) => {
+    this.setState((state) => ({
+      [option]: state[option] + 1,
+    }));
+  };
+
+  countTotalFeedback = () =>
+    Object.values(this.state).reduce((acc, item) => acc + item, 0);
+
+  countPositiveFeedbackPercentage = () =>
+    this.countTotalFeedback() &&
+    Math.round((this.state.good / this.countTotalFeedback()) * 1000) / 10;
+  stateKeys = Object.keys(this.state);
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const totalFeedbacks = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
+
+    return (
+      <Box display="flex" flexDirection="column" alignItems="center" p={5}>
+        <Box
+          textAlign="center"
+          border="normal"
+          borderColor="accent"
+          borderRadius="normal"
+          p={4}
+        >
+          <Section title="Please leave feedback">
+            <FeedbackOptions
+              options={this.stateKeys}
+              onLeaveFeedback={this.handleChosenFeedback}
+            ></FeedbackOptions>
+          </Section>
+        </Box>
+        <Box textAlign="left">
+          <Section title="Statistics">
+            {totalFeedbacks === 0 ? (
+              <Notification message="There is no feedback"></Notification>
+            ) : (
+              <Statistics
+                good={good}
+                neutral={neutral}
+                bad={bad}
+                total={totalFeedbacks}
+                positivePercentage={positivePercentage}
+              ></Statistics>
+            )}
+          </Section>
+        </Box>
+      </Box>
+    );
+  }
+}
